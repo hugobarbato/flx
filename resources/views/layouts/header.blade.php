d6f09c4e467796caecb4af9032f182f9879fab7a
Hugo Barbato <hugobarbato@gmail.com>
2019-10-11 15:01:54 -0300

    ajustando algumas coisas!

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,9 +1,9 @@
 
     <header>
         <div class="container menu-nav">
-            <nav class="navbar navbar-expand-lg navbar-light">
+            <nav class="navbar navbar-expand-lg navbar-light w-100">
                 <div class="logo-flx">
-                    <a class="navbar-brand" href="#"><img src="{{url('img/logo.png')}}"> </a>
+                    <a class="navbar-brand" href="{{url('')}}"><img src="{{url('img/logo.png')}}"> </a>
                 </div>
                 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSite">
@@ -13,16 +13,16 @@
                 <div class="collapse navbar-collapse menu-principal" id="navbarSite">
                     <ul class="navbar-nav">
                        <li class="nav-item {{ (request()->is('home')) ? 'active' : '' }}">
-                            <a class="nav-link" href="/home">Home</a>
+                            <a class="nav-link" href="{{url('/home')}}">Home</a>
                         </li>
                         <li class="nav-item {{ (request()->is('search/rent')) ? 'active' : '' }}">
-                            <a class="nav-link" href="/search/rent">Alugar</a>
+                            <a class="nav-link" href="{{url('/search/rent')}}">Alugar</a>
                         </li>
                         <li class="nav-item {{ (request()->is('search/sell')) ? 'active' : '' }}">
-                            <a class="nav-link" href="/search/sell">Comprar</a>
+                            <a class="nav-link" href="{{url('/search/sell')}}">Comprar</a>
                         </li>
                         <li class="nav-item {{ (request()->is('search/news')) ? 'active' : '' }}">
-                            <a class="nav-link" href="/search/news">Lançamentos</a>
+                            <a class="nav-link" href="{{url('/search/news')}}">Lançamentos</a>
                         </li>
                         <!--<li class="nav-item">-->
                         <!--    <a class="nav-link" href="/comprar">Comprar</a>-->
@@ -32,56 +32,67 @@
                         <!--</li>-->
                         
                         
-                        @guest
-                            <li class="nav-item {{ (request()->is('login')) ? 'active' : '' }}">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Entrar') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item dropdown  {{ (request()->is('/imovel/adicionar')) || (request()->is('/imovel/listar')) ? 'active' : '' }}">
-                                <a id="imoveis" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" 
-                                aria-haspopup="true" aria-expanded="false" v-pre>
-                                    Imóveis <span class="caret"></span>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="imoveis">
-                                    <a class="dropdown-item {{ (request()->is('/imovel/adicionar')) ? 'active' : '' }}" href="{{ url('/imovel/adicionar') }}" >
-                                        Adicionar
-                                    </a>
-                                    <a class="dropdown-item {{ (request()->is('/imovel/listar')) ? 'active' : '' }}" href="{{ url('/imovel/listar') }}" >
-                                        Listar
-                                    </a>
-                                </div>
-                            </li>
-                            
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                        Sair
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
+                        
                         
                     </ul>
                 </div>
+                    @guest
+                        <div class="menu-principal mx-3" >
+                            <ul class="navbar-nav">
+                                <li class="nav-item {{ (request()->is('login')) ? 'active' : '' }}">
+                                    <a class="nav-link" href="{{ route('login') }}"> 
+                                        <i class="fas fa-sign-in-alt"></i>
+                                        {{ __('Entrar') }}
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="float-right nav-anuncio-gratis {{ (request()->is('adesao')) ? 'active' : '' }}">
+                            <a href="{{url('/adesao')}}">Anuncie Grátis por 45 dias</a>
+                        </div> 
+                    @else
+                        <div class="menu-principal" >
+                            <ul class="navbar-nav">
+                                <li class="nav-item dropdown  {{ (request()->is('/imovel/adicionar')) || (request()->is('/imovel/listar')) ? 'active' : '' }}">
+                                    <a id="imoveis" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" 
+                                    aria-haspopup="true" aria-expanded="false" v-pre>
+                                        <i class="fa fa-home" aria-hidden="true"></i> Imóveis <span class="caret"></span>
+                                    </a>
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="imoveis">
+                                        <a class="dropdown-item {{ (request()->is('/imovel/adicionar')) ? 'active' : '' }}" href="{{ url('/imovel/adicionar') }}" >
+                                            Adicionar
+                                        </a>
+                                        <a class="dropdown-item {{ (request()->is('/imovel/listar')) ? 'active' : '' }}" href="{{ url('/imovel/listar') }}" >
+                                            Listar
+                                        </a>
+                                    </div>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        <i class="fa fa-user-circle" aria-hidden="true"></i> {{ Auth::user()->name }} <span class="caret"></span>
+                                    </a>
+
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                            document.getElementById('logout-form').submit();">
+                                            Sair
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        
+                    @endguest
             </nav>  
-            <div class="nav-anuncio-gratis {{ (request()->is('adesao')) ? 'active' : '' }}">
-                <a href="/adesao">Anuncie Grátis por 45 dias</a>
-            </div> 
         </div>
     </header>
-
-
-@if( Auth::user()->is_admin)
+ 
+@if( Auth::user() && Auth::user()->is_admin)
     <a href="{{ url('/admin') }}" class="btn btn-danger btn-admin"> Acessar Painel ADM </a>
     
-@endif
+@endif 
--- resources/views/pacotesAdesao.blade.php
+++ resources/views/pacotesAdesao.blade.php
@@ -9,116 +9,49 @@
                     
                     <div class="row">
                         <div class="col-md-12">
-                            <h4 class="subtitulos-h4">Destaques para venda</h4>
+                            <h4 class="subtitulos-h4 my-3">Destaques para venda</h4>
                         </div>
                     </div>
                     
-                    <div class="promocao-lancamentos">
-                        <p>Promoção de lançamento</p>
-                        <p>25 anúncios <span>grátis</span> por 45 dias</p>
-                    </div>
-                    <div class="detalhe-adesao">
-                        <p>Promoção válida para 25 anúncios simples ou 45 dias, valendo o que ocorrer primeiro.</p>
-                        <p>Não é válida para proprietários diretos</p>
-                    </div>
+                    <label class="pacote-free w-100 m-0">
+                        <input type="radio" name="pacote_selected" id="pacote_selected" value="FREE" style="display:none">
+                        <div class="card-pacote">
+                            <div class="promocao-lancamentos">
+                                <p>Promoção de lançamento</p>
+                                <p>25 anúncios <span>grátis</span> por 45 dias</p>
+                            </div>
+                            <div class="detalhe-adesao">
+                                <p>Promoção válida para 25 anúncios simples ou 45 dias, valendo o que ocorrer primeiro.</p>
+                                <p>Não é válida para proprietários diretos</p>
+                            </div>
+                        </div>
+                    </label>
                     
                     <div class="cards-planos">
                         <div class="row">
                         @foreach($pacotes as $pacote)
-                                <div class="col-md-4">
-                                    <label>
-                                    <input type="radio" name="pacote_selected" id="pacote_selected" style="display:none">
-                                    <div class="card">
-                                        <h4 class="card-header">{{$pacote->nm_titulo}}</h4>
-                                        <div class="card-body">
-                                            <p><span class="vl-anuncio">R$ {{ number_format($pacote->vl_pacote,2,',',' ') }}</span> / mês</p>
-                                            <div class="detalhes-planos">
-                                                @if($pacote->qt_anuncio > 0 )
-                                                <p><span class="qt-anuncios">{{$pacote->qt_anuncio}} anúncios</span></p>
-                                                @else
-                                                <p><span class="qt-anuncios">anúncios ilimitados</span></p>
-                                                @endif    
-                                                <p>inclui {{$pacote->qt_destaques}} destaque</p>
-                                            </div>
+                            <div class="col-md-4">
+                                <label>
+                                <input type="radio" name="pacote_selected" id="pacote_selected" value="{{$pacote->cd_pacote}}" style="display:none">
+                                <div class="card">
+                                    <h4 class="card-header">{{$pacote->nm_titulo}}</h4>
+                                    <div class="card-body">
+                                        <p><span class="vl-anuncio">R$ {{ number_format($pacote->vl_pacote,2,',',' ') }}</span> / mês</p>
+                                        <div class="detalhes-planos">
+                                            @if($pacote->qt_anuncio > 0 )
+                                            <p><span class="qt-anuncios">{{$pacote->qt_anuncio}} anúncios</span></p>
+                                            @else
+                                            <p><span class="qt-anuncios">anúncios ilimitados</span></p>
+                                            @endif    
+                                            <p>inclui {{$pacote->qt_destaques}} destaque</p>
                                         </div>
-                                        <div class="card-footer">Somente R$ {{ number_format(($pacote->vl_pacote/30),2,',',' ') }} por dia</div>
                                     </div>
-                                    </label>
-                                </div>                 
+                                    <div class="card-footer">Somente R$ {{ number_format(($pacote->vl_pacote/30),2,',',' ') }} por dia</div>
+                                </div>
+                                </label>
+                            </div>                 
                         @endforeach
                         </div>
-<!--                         
-                        <div class="col-md-4">
-                                    <div class="card">
-                                        <h4 class="card-header">plano 25</h4>
-                                        <div class="card-body">
-                                            <p><span class="vl-anuncio">R$ 59,90</span> / mês</p>
-                                            <div class="detalhes-planos">
-                                                <p><span class="qt-anuncios">25 anúncios</span></p>
-                                                <p>inclui 03 destaques</p>
-                                            </div>
-                                        </div>
-                                        <div class="card-footer">Somente R$ 2,00 por dia</div>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-md-4">
-                                    <div class="card">
-                                        <h4 class="card-header">plano 50</h4>
-                                        <div class="card-body">
-                                            <p><span class="vl-anuncio">R$ 79,90</span> / mês</p>
-                                            <div class="detalhes-planos">
-                                                <p><span class="qt-anuncios">50 anúncios</span></p>
-                                                <p>inclui 06 destaques</p>
-                                            </div>
-                                        </div>
-                                        <div class="card-footer">Somente R$ 2,00 por dia</div>
-                                    </div>
-                                </div> -->
-                        <!-- <div class="row">
-                            <div class="col-md-4">
-                                <div class="card">
-                                    <h4 class="card-header">plano 100</h4>
-                                    <div class="card-body">
-                                        <p><span class="vl-anuncio">R$ 99,90</span> / mês</p>
-                                        <div class="detalhes-planos">
-                                            <p><span class="qt-anuncios">100 anúncios</span></p>
-                                            <p>inclui 10 destaques</p>
-                                        </div>
-                                    </div>
-                                    <div class="card-footer">Somente R$ 3,33 por dia</div>
-                                </div>
-                            </div>
-                            
-                            <div class="col-md-4">
-                                <div class="card">
-                                    <h4 class="card-header">plano 250</h4>
-                                    <div class="card-body">
-                                        <p><span class="vl-anuncio">R$ 199,90</span> / mês</p>
-                                        <div class="detalhes-planos">
-                                            <p><span class="qt-anuncios">250 anúncios</span></p>
-                                            <p>inclui 25 destaques</p>
-                                        </div>
-                                    </div>
-                                    <div class="card-footer">Somente R$ 6,00 por dia</div>
-                                </div>
-                            </div>
-                            
-                            <div class="col-md-4">
-                                <div class="card">
-                                    <h4 class="card-header">plano ilimitado</h4>
-                                    <div class="card-body">
-                                        <p><span class="vl-anuncio">R$ 499,90</span> / mês</p>
-                                        <div class="detalhes-planos">
-                                            <p><span class="qt-anuncios">anúncios ilimitados</span></p>
-                                            <p>inclui 50 destaques</p>
-                                        </div>
-                                    </div>
-                                    <div class="card-footer">Somente R$ 13,33 por dia</div>
-                                </div>
-                            </div>
-                            
-                        </div> -->
                     </div>
                 </div>
                 
@@ -137,23 +70,19 @@
                                 <div class="destaque-anuncio-info">
                                     
                                      <div>
-                                        <input type="radio" name="scales" checked>
-                                        <label for="scales">Compre 10 destaques - R$100,00 </label>
+                                        <label for="scales"> <input type="radio" name="scales" > Compre 10 destaques - R$100,00 </label>
+                                    </div>
+                                    
+                                    <div> 
+                                        <label for="scales"> <input type="radio" name="scales"> Compre 25 destaques - R$200,00</label>
+                                    </div>
+                                    
+                                    <div> 
+                                        <label for="scales"> <input type="radio" name="scales"> Compre 50 destaques - R$350,00</label>
                                     </div>
                                     
                                     <div>
-                                        <input type="radio" name="scales">
-                                        <label for="scales">Compre 25 destaques - R$200,00</label>
-                                    </div>
-                                    
-                                    <div>
-                                        <input type="radio" name="scales">
-                                        <label for="scales">Compre 50 destaques - R$350,00</label>
-                                    </div>
-                                    
-                                    <div>
-                                        <input type="radio" name="scales">
-                                        <label for="scales">Compre 100 destaques - R$600,00</label>
+                                        <label for="scales"> <input type="radio" name="scales"> Compre 100 destaques - R$600,00</label>
                                     </div> 
                         
                                 </div>
@@ -164,24 +93,21 @@
                                 
                                 <div class="destaque-anuncio-info">
                                     
-                                    <div>
-                                        <input type="radio" name="scalesSuper" checked>
-                                        <label for="scales">Compre 01 destaque - R$100,00 </label>
+                                    <div> 
+                                        <label for="scales"> <input type="radio" name="scalesSuper"> Compre 01 destaque - R$100,00 </label>
+                                    </div>
+                                    
+                                    <div> 
+                                        <label for="scales"> <input type="radio" name="scalesSuper"> Compre 03 destaques - R$250,00</label>
                                     </div>
                                     
                                     <div>
-                                        <input type="radio" name="scalesSuper">
-                                        <label for="scales">Compre 03 destaques - R$250,00</label>
+                                        
+                                        <label for="scales"> <input type="radio" name="scalesSuper"> Compre 06 destaques - R$500,00</label>
                                     </div>
                                     
-                                    <div>
-                                        <input type="radio" name="scalesSuper">
-                                        <label for="scales">Compre 06 destaques - R$500,00</label>
-                                    </div>
-                                    
-                                    <div>
-                                        <input type="radio" name="scalesSuper">
-                                        <label for="scales">Compre 12 destaques - R$1.000,00</label>
+                                    <div> 
+                                        <label for="scales"> <input type="radio" name="scalesSuper"> Compre 12 destaques - R$1.000,00</label>
                                     </div> 
                                 </div>
                             </div>
@@ -217,13 +143,11 @@
                     
                     <div class="row">
                         <div class="col-md-12">
-                            <div class="pagamento-icon pagseguro-card"> 
-                                    <img class="img-pagseguro-adesao" src="img/pagseguro.jpg"> 
+                            <div class="pagamento-icon pagseguro-card pointer"> 
+                                    <img class="img-pagseguro-adesao" src="img/pagseguro.jpg" onclick="PagSeguroRedirect()"> 
                             </div>
                         </div>
                     </div>
-                    
-                    
                     
                 </div>
                 
@@ -232,8 +156,7 @@
     </article>
 @endsection
 @section('scripts')
-<script type="text/javascript" src="{{ asset('js/imob.js') }}"></script>
-
+    <script type="text/javascript" src="{{ asset('js/imob.js') }}"></script>
 @endsection
 @section('styles')
 <style>
@@ -244,11 +167,13 @@
     margin-bottom: 50px;
     width: fit-content;
 }
-.cards-planos label {
+
+.cards-planos label, .pacote-free  {
     display: block;
     width: 100%;
 }
-.cards-planos input:checked + div.card {
+.cards-planos input:checked + div.card, 
+.pacote-free input:checked + div.card-pacote {
     border: 2px solid red;
 }
 </style>
